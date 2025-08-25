f01c517d8aac58eb4c474aafa794428621284b4e
Jp23 <yhperez2@gmail.com>
2025-08-24 20:38:05 -0500

    Nuevo java

---
+++ EjemploVariables.java
@@ -0,0 +1,22 @@
+
+// Una variable es un espacio en la memoria del computador que tiene un nombre y guarda un valor, 
+// el cual puede cambiar mientras el programa se ejecuta.
+
+public class EjemploVariables {
+    public static void main(String[] args) {
+        int numero = 10;               // almacena un número entero
+        double precio = 99.99;         // almacena un número decimal
+        String saludo = "Hola mundo";  // almacena texto
+        boolean encendido = false;     // almacena verdadero o falso
+        int numero2= 15;
+        int suma= numero+numero2;
+
+        System.out.println(numero);
+        System.out.println(precio);
+        System.out.println(saludo);
+        System.out.println(encendido);
+        System.out.println(numero2);
+        System.out.println("La suma es : "+suma);
+    }
+}
+
